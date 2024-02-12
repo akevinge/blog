96ec3cda5ba2861a458662689bb6842893f633d5
Kevin Ge <ge.kevin.a@gmail.com>
2024-02-12 16:37:20 -0600

    fix: remove tags and fix CI errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,5 @@
 import { z, defineCollection } from "astro:content";
 
-function removeDupsAndLowerCase(array: string[]) {
-	if (!array.length) return array;
-	const lowercaseItems = array.map((str) => str.toLowerCase());
-	const distinctItems = new Set(lowercaseItems);
-	return Array.from(distinctItems);
-}
-
 const post = defineCollection({
 	type: "content",
 	schema: ({ image }) =>
